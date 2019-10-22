fcc81a423219f3ab1c3e502d04bc8ad66d585a8b
Wesley Moore <wes@wezm.net>
2019-10-23 08:10:26 +1100

    Use newly updated DEFAULT ruleset

--- src/sanitise.rs
+++ src/sanitise.rs
@@ -1,44 +1,7 @@
-use lazy_static::lazy_static;
-use sanitize_html::{errors::SanitizeError, rules::Rules, sanitize_str};
-
-lazy_static! {
-    pub static ref RULES: Rules = Rules::new()
-        .space("address")
-        .space("article")
-        .space("aside")
-        .space("blockquote")
-        .space("br")
-        .space("dd")
-        .space("div")
-        .space("dl")
-        .space("dt")
-        .space("footer")
-        .space("h1")
-        .space("h2")
-        .space("h3")
-        .space("h4")
-        .space("h5")
-        .space("h6")
-        .space("header")
-        .space("hgroup")
-        .space("hr")
-        .space("li")
-        .space("nav")
-        .space("ol")
-        .space("p")
-        .space("pre")
-        .space("section")
-        .space("ul")
-        .delete("iframe")
-        .delete("noembed")
-        .delete("noframes")
-        .delete("noscript")
-        .delete("script")
-        .delete("style");
-}
+use sanitize_html::{errors::SanitizeError, rules::predefined::DEFAULT, sanitize_str};
 
 pub(crate) fn strip_tags(input: &str) -> Result<String, SanitizeError> {
-    sanitize_str(&RULES, input)
+    sanitize_str(&DEFAULT, input)
 }
 
 #[cfg(test)]
